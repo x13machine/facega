c5a850ca6ffe469a48609809ce4725e922ddf86b
x13machine <brandanw2@gmail.com>
2018-10-16 04:11:48 -0500

    host change

--- facega/settings.py
+++ facega/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f_26zo2a*xj+l68xh)dzibo&*$gifwx9_$fw$4d2qi^lo57oyb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('debug', 'true') == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+	'facega.demos.brandanwaldvogel.com',
+	'facega-demos.brandanwaldvogel.com',
+	'192.168.1.105'
+]
 
 
 # Application definition
