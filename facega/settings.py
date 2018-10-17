c5cb6dd35ab4d1c9fd7ae1bcec7c651c95dfbb42
x13machine <brandanw2@gmail.com>
2018-10-16 22:45:12 -0500

    statics files to google cloud

--- facega/settings.py
+++ facega/settings.py
@@ -130,7 +130,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.getenv('static','/static/')
+
 STATICFILES_DIRS = [
 	os.path.join(BASE_DIR, 'www/dist'),
 ]
+
+
+STATIC_ROOT = 'staticfiles'
