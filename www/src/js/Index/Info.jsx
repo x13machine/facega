ca887d7c078372f8c4f28838ffa0576c3a6331fb
x13machine <brandanw2@gmail.com>
2018-10-25 00:03:36 -0500

    hide about

--- www/src/js/Index/Info.jsx
+++ www/src/js/Index/Info.jsx
@@ -7,7 +7,9 @@ class Info extends Component {
 		return (
 			<React.Fragment>
 				<h1>FaceGA</h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempus, justo sit amet bibendum hendrerit, orci ipsum tempus enim, in condimentum erat elit ac tellus. Etiam tellus nulla, dapibus in tempor in, feugiat eget sapien. Vestibulum rutrum ex leo, non molestie tellus vulputate facilisis. Aliquam ipsum tellus, luctus quis volutpat ac, fermentum et dolor. Ut non nisl sed massa porttitor dictum. In vehicula gravida velit at dignissim. Curabitur nec maximus quam. Ut tempus neque quis dui venenatis, eget fringilla nulla molestie. Etiam finibus, nulla sed eleifend efficitur, tellus dolor ultricies felis, at sagittis est odio vitae lectus. Sed at lobortis turpis. Fusce eget venenatis dolor. Morbi ac scelerisque ante.</p>
+				<p>
+				FaceGA is an experiment I'm made to use a genetic algorithm to find the face you find most attractive. You select faces until you are satisfied with the results. The Faces are produced by an autoencoder. I probably need to improve the autoencoder.
+				</p>
 				<button className='btn btn-primary info-button' onClick={() => this.paras.jump('Evolve', {type: 'start', gender: 'f'})}>Female Autoencoder</button>
 				<button className='btn btn-primary info-button' onClick={() => this.paras.jump('Evolve', {type: 'start', gender: 'm'})}>Male Autoencoder</button>
 			</React.Fragment>
